462aa50a5a5a23cee11e76ea79da132cd3d421b5
Alonso R <luis.alonso.16@hotmail.com>
2020-08-10 18:42:35 -0600

    Add initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Social-behavior fluctuation peak detector in a relativistic exponential environment using data science.',
+    author='maestre3d',
+    license='',
+)
